d1f6cda66d1b93f94be095e45d535781e6b73d5d
Bardh Dajaku <bd58352@ubt-uni.net>
2026-03-11 00:59:05 +0100

    fix car routes gallery handler

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -8,7 +8,6 @@ router.get("/cars", carController.getCars);
 router.get("/cars/:id/gallery", carController.getCarGallery);
 
 router.post("/cars", upload.single("main_image"), carController.addCar);
-router.post("/cars/:id/gallery", upload.array("images", 10), carController.uploadGallery);
 
 router.delete("/cars/:id", carController.deleteCar);
 router.put("/cars/:id", upload.single("main_image"), carController.updateCar);
